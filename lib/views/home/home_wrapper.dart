e45e367414a3c227ef1517aeb9de407787531998
mertozanlislas <ozanlislas66@gmail.com>
2025-04-24 00:45:29 +0300

    added home wrapper and empty home page. configured with firebase auth

---
+++ lib/views/home/home_wrapper.dart
@@ -0,0 +1,30 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:paylas/services/auth/auth_service.dart';
+import 'package:paylas/views/home/home_page.dart';
+import 'package:paylas/views/login/login_page.dart';
+
+class HomeWrapper extends StatelessWidget {
+  final AuthService _authService = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: _authService.authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        if (!snapshot.hasData) {
+          return LoginPage();
+        }
+
+        return HomePage();
+      },
+    );
+  }
+}
--- lib/views/login/login_page.dart
+++ lib/views/login/login_page.dart
@@ -1,17 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:paylas/locator/locator.dart';
 import 'package:paylas/tools/screen_sizes.dart';
+import 'package:paylas/views/home/home_wrapper.dart';
 import 'package:paylas/views/login/bottom_bar.dart';
 import 'package:paylas/views/login/input_bar.dart';
 import 'package:paylas/views/widgets/login_header.dart';
 import 'package:paylas/views/widgets/plane_icon.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final screen = locator<ScreenSizes>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user != null) {
+        // Kullanıcı zaten giriş yapmış, direkt HomePage'e yönlendir
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => HomeWrapper()),
+          );
+        });
+      }
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,23 +51,23 @@ class LoginPage extends StatelessWidget {
           child: Stack(
             children: [
               Positioned(
-        
+
                 left: screen.width * 0.075,
                 top: screen.height * 0.09,
-        
+
                 child: LoginHeader()
               ),
-        
+
               LoginInputBar(
                 emailController: emailController,
                 passwordController: passwordController,
               ),
-        
+
               LoginBottomBar(
                 emailController: emailController,
                 passwordController: passwordController,
               ),
-        
+
               Positioned(
                 top: 20,
                 right: 50,
